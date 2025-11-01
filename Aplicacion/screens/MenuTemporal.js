a705d15e6dfb2c2f2191fbeea4ab31032aa61a82
José Luis Huante Ríos <124051243@upq.edu.mx>
2025-10-31 20:03:52 -0600

    Creación de Menú Temporal y de la primera Screen para los presupuestos.
    Se creó un menú temporal para ir visualizando todas las Screens sin funcionalidad todavía, además también se creó la primera screen donde irán los presupuestos. Nota: para el componente de "SafeAreaView" fue necesaria instalar la siguiente librería: npm install react-native-safe-area-context

---
+++ Aplicacion/screens/MenuTemporal.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Button, ScrollView } from 'react-native';
+import PresupuestoScreen from './PresupuestoScreen';
+
+export default function MenuTemporal() {
+  const [screen, setScreen] = useState('menu');
+
+  switch (screen) {
+    case 'inicioDeSesion':
+      return <InicioDeSesionScreen />;
+
+    case 'presupuesto':
+      return <PresupuestoScreen />;
+
+    default:
+      return (
+        <View style={styles.container}>
+          <Text>Menú temporal para visualizar nuestras interfaces</Text>
+          <ScrollView>
+            <View style={styles.contenedorBotones}>
+              <Button
+                onPress={() => setScreen('inicioDeSesion')}
+                title="Screen Inicio De Sesión"
+              />
+              <Button
+                onPress={() => setScreen('presupuesto')}
+                title="Screen de Presupuesto"
+              />
+              
+            </View>
+          </ScrollView>
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contenedorBotones: {
+    marginTop: 25,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#f5f5f5',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.15,
+    shadowRadius: 6,
+    elevation: 4,
+  },
+});
